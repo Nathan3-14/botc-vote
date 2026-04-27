4098c75d503c0c2a73b9a2e6538829a3d1a923f5
Nathan <nathan.watson.172@gmail.com>
2026-04-27 21:52:47 +0100

    improved filtering

--- server/static/js/filter.js
+++ server/static/js/filter.js
@@ -1,6 +1,6 @@
 const filter_options = (filter_value) => {
     var options = document.getElementsByName("script");
-    var filter_value = filter_value.toLowerCase();
+    var filter_value = filter_value.toLowerCase().replace(" ", "_");
     for (const option of options) {
         var option_value = option.children[0].value.toLowerCase();
         if (option_value.includes(filter_value)) {
